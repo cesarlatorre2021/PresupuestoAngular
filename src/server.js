57d06542e687122a39be46a0d12e68dbcc925a0f
Cesar Latorre <sfecesar1941@hotmail.com>
2021-09-09 09:43:16 -0500

    Nuevos ajustes

--- src/server.js
+++ src/server.js
@@ -8,4 +8,6 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/proyectoAngPresupuesto/index.html'));
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
