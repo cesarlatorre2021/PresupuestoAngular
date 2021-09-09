d85cb22d78be7ec02df9eaa1ed7b452e0fef7754
Cesar Latorre <sfecesar1941@hotmail.com>
2021-09-09 09:29:55 -0500

    Se agrega el archuvo Procfile para solucionar errores en Heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/proyectoAngPresupuesto'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/proyectoAngPresupuesto/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5000);
